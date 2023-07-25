d9ede3e76f18244ce347ceb782970c238aaadbae
apiusage <81261727+apiusage@users.noreply.github.com>
2023-07-25 23:22:11 +0800

    Add files via upload

--- potentialCoinAnalyzer.py
+++ potentialCoinAnalyzer.py
@@ -11,7 +11,7 @@ def run_potentialCoin():
     st.header("__Potential Coin__")
     intervals = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
 
-    marketpairsList = []
+    marketpairsList, dataMarketPairs = [], ''
     dataMarketPairs = GetMarketPairs()
     for i in range(0, len(dataMarketPairs['symbols'])):
         marketpairsList.append(dataMarketPairs['symbols'][i]['symbol'])
